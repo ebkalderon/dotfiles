7f09857a90423edd61e53c80f7a1b14164b5eb80
Eyal Kalderon <ebkalderon@gmail.com>
2016-07-05 18:53:47 -0400

    Add subversion support to prompt

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -2,16 +2,32 @@
 # bash/prompt.sh
 #
 
-# If in a Git repository, display branch
-_prompt() {
-    BRANCH=`git rev-parse --abbrev-ref HEAD 2> /dev/null`
+_branch_git() {
+    git rev-parse --abbrev-ref HEAD 2> /dev/null
+}
 
-    if [ $? -eq 0 ]; then
-        echo -e "$txtblu\u$txtrst@$txtgrn\h $txtred\W $txtpur$BRANCH" \
+_branch_svn() {
+    svn info 2> /dev/null | grep '^URL:' | \
+        grep -Eo '(tags|branches)/[^/]+|trunk' | grep -Eo '[^/]+$' | \
+        sed 'N;s/\n/\//'
+}
+
+_prompt() {
+    GIT_BRANCH=`_branch_git`
+    if [ $GIT_BRANCH ]; then
+        echo -e "$txtblu\u$txtrst@$txtgrn\h $txtred\W $txtpur$GIT_BRANCH" \
                 "$txtylw\\\$ $txtrst"
-    else
-        echo -e "$txtblu\u$txtrst@$txtgrn\h $txtred\W $txtylw\\\$ $txtrst"
+        return
     fi
+
+    SVN_BRANCH=`_branch_svn`
+    if [ $SVN_BRANCH ]; then
+        echo -e "$txtblu\u$txtrst@$txtgrn\h $txtred\W $txtpur$SVN_BRANCH" \
+                "$txtylw\\\$ $txtrst"
+        return
+    fi
+
+    echo -e "$txtblu\u$txtrst@$txtgrn\h $txtred\W $txtylw\\\$ $txtrst"
 }
 
 PROMPT_COMMAND='PS1=$(_prompt)'
