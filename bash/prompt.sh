5d9d7f7239ae73c2227c70b5374ebaa4bf93bbd9
Eyal Kalderon <ebkalderon@users.noreply.github.com>
2016-07-07 03:49:37 -0400

    Add comments to prompt.sh

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -2,22 +2,26 @@
 # bash/prompt.sh
 #
 
+# Executes VTE-specific functions, if available
 _is_vte_term() {
     if [[ -f /etc/profile.d/vte.sh ]] && [[ -n "$VTE_VERSION" ]]; then
         __vte_prompt_command
     fi
 }
 
+# Returns the current Git branch name (example: `master`)
 _branch_git() {
     git rev-parse --abbrev-ref HEAD 2> /dev/null
 }
 
+# Returns the current Subversion branch name (example: `ekalderon/trunk`)
 _branch_svn() {
     svn info 2> /dev/null | grep '^URL:' | \
         grep -Eo '(tags|branches)/[^/]+|trunk' | grep -Eo '[^/]+$' | \
         sed 'N;s/\n/\//'
 }
 
+# Builds the PS1 prompt, optionally with the current Git/Subversion branch name
 _prompt() {
     GIT_BRANCH=`_branch_git`
     if [ -n "$GIT_BRANCH" ]; then
