4fcf3f4fa72ffcea083fccb8aae6896018d7e0ff
Eyal Kalderon <ebkalderon@gmail.com>
2018-07-17 13:14:30 +0800

    Export FZF_DEFAULT_COMMAND for better performance in Git repos

--- autoload/fzf.sh
+++ autoload/fzf.sh
@@ -9,3 +9,9 @@ if [[ -d /usr/share/fzf ]]; then
 elif [[ -f ~/.fzf.bash ]]; then
   source ~/.fzf.bash
 fi
+
+# Use `git ls-tree` if running in Git repository, use `find` otherwise
+export FZF_DEFAULT_COMMAND='
+  (git ls-tree -r --name-only HEAD ||
+   find . -path "*/\.*" -prune -o -type f -print -o -type l -print |
+      sed s/^..//) 2> /dev/null'
