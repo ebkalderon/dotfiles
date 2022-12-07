c657afffbf234baf535aad63edbfd0387f15c66a
Eyal Kalderon <ebkalderon@gmail.com>
2022-12-06 20:37:16 -0500

    Clean up LSP and DAP keymaps in Neovim

--- packages/configs/files/home/.config/nvim/lua/user/keymaps.lua
+++ packages/configs/files/home/.config/nvim/lua/user/keymaps.lua
@@ -38,43 +38,35 @@ keymap("n", "<C-j>", ":Telescope project<CR>", opts)
 -- Language server shortcuts
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
-    local bufnr = args.buf
-
     -- Enable completion triggered by <C-space>
-    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+    vim.api.nvim_buf_set_option(args.buf, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-    vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set("n", "<leader>wl", function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "gp", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "gn", vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
+
+    local bufopts = { noremap = true, silent = true, buffer = args.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<leader>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workleader_folders()))
     end, bufopts)
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
-    vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format({ async = true }) end, bufopts)
-    vim.keymap.set("n", "<leader>d", function()
-      vim.diagnostic.open_float(bufnr, { focusable = false, severity_sort = false })
-    end, bufopts)
-    vim.keymap.set("n", "gp", vim.diagnostic.goto_prev, bufopts)
-    vim.keymap.set("n", "gn", vim.diagnostic.goto_next, bufopts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 
     vim.keymap.set("n", "<F4>", function() require("dapui").toggle({ reset = true }) end, bufopts)
     vim.keymap.set("n", "<F5>", function() require("dap").continue() end, bufopts)
     vim.keymap.set("n", "<F17>", function() require("dap").close() end, bufopts) -- <S-F5>
     vim.keymap.set("n", "<F9>", function() require("dap").toggle_breakpoint() end, bufopts)
-    vim.keymap.set("n", "<F21>", function()
-      require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-    end, bufopts) -- <S-F9>
-    vim.keymap.set("n", "<F10>", function()
-      require("dap").set_breakpoint({ nil, nil, vim.fn.input('Log point message: ') })
-    end, bufopts)
     vim.keymap.set("n", "<F1>", function() require("dap").step_over() end, bufopts)
     vim.keymap.set("n", "<F2>", function() require("dap").step_into() end, bufopts)
     vim.keymap.set("n", "<F14>", function() require("dap").step_out() end, bufopts) -- <S-F2>
