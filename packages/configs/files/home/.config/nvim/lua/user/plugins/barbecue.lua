30a4ea032e2e6cbdaa6b87046079d0029650d1b6
Eyal Kalderon <ebkalderon@gmail.com>
2022-12-21 00:47:07 -0500

    Apply custom highlight groups to barbecue.nvim

--- packages/configs/files/home/.config/nvim/lua/user/plugins/barbecue.lua
+++ packages/configs/files/home/.config/nvim/lua/user/plugins/barbecue.lua
@@ -9,73 +9,54 @@ require("barbecue").setup({
 })
 
 local background = "#272822"
-vim.api.nvim_set_hl(0, "WinBar", { bg = background })
+vim.api.nvim_set_hl(0, "BarbecueNormal", { bg = background })
 vim.api.nvim_set_hl(0, "BarbecueBasename", { fg = "#f8f8f2", bg = background })
 vim.api.nvim_set_hl(0, "BarbecueContext", { fg = "#bcbcbc", bg = background })
 vim.api.nvim_set_hl(0, "BarbecueDirname", { fg = "#75715e", bg = background })
 vim.api.nvim_set_hl(0, "BarbecueEllipsis", { link = "BarbecueDirname" })
 vim.api.nvim_set_hl(0, "BarbecueModified", { fg = "#e6db74", bg = background })
 vim.api.nvim_set_hl(0, "BarbecueSeparator", { link = "BarbecueDirname" })
---
--- local highlights = {
---   "BarbecueContextFile",
---   "BarbecueContextModule",
---   "BarbecueContextNamespace",
---   "BarbecueContextPackage",
---   "BarbecueContextClass",
---   "BarbecueContextMethod",
---   "BarbecueContextProperty",
---   "BarbecueContextField",
---   "BarbecueContextConstructor",
---   "BarbecueContextEnum",
---   "BarbecueContextInterface",
---   "BarbecueContextFunction",
---   "BarbecueContextVariable",
---   "BarbecueContextConstant",
---   "BarbecueContextString",
---   "BarbecueContextNumber",
---   "BarbecueContextBoolean",
---   "BarbecueContextArray",
---   "BarbecueContextObject",
---   "BarbecueContextKey",
---   "BarbecueContextNull",
---   "BarbecueContextEnumMember",
---   "BarbecueContextStruct",
---   "BarbecueContextEvent",
---   "BarbecueContextOperator",
---   "BarbecueContextTypeParameter",
--- }
---
--- for _, name in pairs(highlights) do
---   local highlight = vim.api.nvim_get_hl_by_name(name, true)
---
---   highlight.fg = highlight.foreground
---   highlight.foreground = nil
---   highlight.bg = background
---   highlight.background = nil
---
---   if highlight[true] ~= nil then
---     highlight = vim.api.nvim_get_hl_by_id(highlight[true], true)
---   end
---
---   vim.api.nvim_set_hl(0, name, highlight)
--- end
---
--- vim.api.nvim_create_autocmd("FileType", {
---   group = vim.api.nvim_create_augroup("Hello", { clear = true }),
---   pattern = "*",
---   callback = function()
---     local bufnr = vim.api.nvim_get_current_buf()
---     local _, name = require("nvim-web-devicons").get_icon_by_filetype(vim.bo[bufnr].filetype)
---     if name ~= nil then
---       local highlight = vim.api.nvim_get_hl_by_name(name, true)
---
---       highlight.fg = highlight.foreground
---       highlight.foreground = nil
---       highlight.bg = "#272822"
---       highlight.background = nil
---
---       vim.api.nvim_set_hl(0, name, highlight)
---     end
---   end,
--- })
+
+local highlights = {
+  "BarbecueContextFile",
+  "BarbecueContextModule",
+  "BarbecueContextNamespace",
+  "BarbecueContextPackage",
+  "BarbecueContextClass",
+  "BarbecueContextMethod",
+  "BarbecueContextProperty",
+  "BarbecueContextField",
+  "BarbecueContextConstructor",
+  "BarbecueContextEnum",
+  "BarbecueContextInterface",
+  "BarbecueContextFunction",
+  "BarbecueContextVariable",
+  "BarbecueContextConstant",
+  "BarbecueContextString",
+  "BarbecueContextNumber",
+  "BarbecueContextBoolean",
+  "BarbecueContextArray",
+  "BarbecueContextObject",
+  "BarbecueContextKey",
+  "BarbecueContextNull",
+  "BarbecueContextEnumMember",
+  "BarbecueContextStruct",
+  "BarbecueContextEvent",
+  "BarbecueContextOperator",
+  "BarbecueContextTypeParameter",
+}
+
+for _, name in pairs(highlights) do
+  local highlight = vim.api.nvim_get_hl_by_name(name, true)
+
+  if highlight[true] ~= nil then
+    highlight = vim.api.nvim_get_hl_by_id(highlight[true], true)
+  end
+
+  highlight.fg = highlight.foreground
+  highlight.foreground = nil
+  highlight.bg = background
+  highlight.background = nil
+
+  vim.api.nvim_set_hl(0, name, highlight)
+end
