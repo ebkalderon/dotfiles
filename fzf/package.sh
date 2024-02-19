fcddd0960230811d5d56c0dabd3d2d95b93f07e5
Eyal Kalderon <ebkalderon@gmail.com>
2024-02-19 15:00:13 -0500

    Replace install/uninstall scripts with dotfiles script
    This commit introduces a tiny single-file package manager script and
    reorganizes my existing dotfiles into declarative packages.

---
+++ fzf/package.sh
@@ -0,0 +1,6 @@
+function config_cleanup() {
+    if [ -d ~/.bashrc.d ]; then
+        find ~/.bashrc.d -xtype l -delete -print
+        rmdir --ignore-fail-on-non-empty ~/.bashrc.d || true
+    fi
+}
