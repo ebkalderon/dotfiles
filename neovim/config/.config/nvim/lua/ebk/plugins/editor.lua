529d2835a1e39207859fc70cddc9c8ee06564c37
Eyal Kalderon <ebkalderon@gmail.com>
2024-02-22 01:17:30 -0500

    Pin neo-tree.nvim to 3.x, clean up config

--- neovim/config/.config/nvim/lua/ebk/plugins/editor.lua
+++ neovim/config/.config/nvim/lua/ebk/plugins/editor.lua
@@ -22,10 +22,12 @@ return {
 
   {
     "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
     cmd = "Neotree",
     dependencies = {
       "MunifTanjim/nui.nvim",
       "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
     },
     keys = {
       {
@@ -46,38 +48,18 @@ return {
       },
     },
     opts = {
+      sources = {
+        "filesystem",
+        "buffers",
+        "git_status",
+        "document_symbols",
+      },
       close_if_last_window = true,
       source_selector = {
         winbar = true,
         content_layout = "center",
         tabs_layout = "equal",
         show_separator_on_edge = true,
-        sources = {
-          { source = "filesystem", display_name = "  Files " },
-          { source = "buffers", display_name = "  Bufs" },
-          { source = "git_status", display_name = " 󰊢 Git " },
-          { source = "document_symbols", display_name = "  Symbols " },
-        },
-      },
-      document_symbols = {
-        kinds = {
-          File = { icon = "󰈙", hl = "Tag" },
-          Namespace = { icon = "󰌗", hl = "Include" },
-          Package = { icon = "󰏖", hl = "Label" },
-          Class = { icon = "󰌗", hl = "Include" },
-          Property = { icon = "󰆧", hl = "@property" },
-          Enum = { icon = "󰒻", hl = "@number" },
-          Function = { icon = "󰊕", hl = "Function" },
-          String = { icon = "󰀬", hl = "String" },
-          Number = { icon = "󰎠", hl = "Number" },
-          Array = { icon = "󰅪", hl = "Type" },
-          Object = { icon = "󰅩", hl = "Type" },
-          Key = { icon = "󰌋", hl = "" },
-          Struct = { icon = "󰌗", hl = "Type" },
-          Operator = { icon = "󰆕", hl = "Operator" },
-          TypeParameter = { icon = "󰊄", hl = "Type" },
-          StaticMethod = { icon = '󰠄 ', hl = 'Function' },
-        }
       },
       default_component_configs = {
         indent = {
