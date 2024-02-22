999b763dd42977af654bfbd89a742670b428604e
Eyal Kalderon <ebkalderon@gmail.com>
2024-02-22 02:59:15 -0500

    Clean up config_cleanup() hooks for all packages
    There's no need to manually delete dangling symlinks, as GNU stow should
    be able to handle this stuff for us.

--- bash/package.sh
+++ bash/package.sh
@@ -1,6 +1,5 @@
 function config_cleanup() {
-    if [ -d ~/.bashrc.d ]; then
-        find ~/.bashrc.d -xtype l -delete -print
+    if [[ -d ~/.bashrc.d ]]; then
         rmdir --ignore-fail-on-non-empty ~/.bashrc.d || true
     fi
 }
