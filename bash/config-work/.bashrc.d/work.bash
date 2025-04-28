da604a4f640f2b76987022598f9453de487667e0
Eyal Kalderon <ebkalderon@gmail.com>
2025-04-28 17:53:47 -0400

    Update 'config-work' package

--- bash/config-work/.bashrc.d/work.bash
+++ bash/config-work/.bashrc.d/work.bash
@@ -2,11 +2,14 @@
 # ~/.bashrc.d/work.bash
 #
 
-export LIBCLANG_PATH=/usr/lib/llvm-10/lib
+export LIBCLANG_PATH=/usr/lib/llvm-18/lib
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
 
 if [ -z "$RUSTUP_HOME" ]; then
   export RUSTUP_HOME="$HOME/.rustup"
