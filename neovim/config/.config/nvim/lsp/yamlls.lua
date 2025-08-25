0086323331f4ba8603d09818c8ed163bffb8414f
Eyal Kalderon <ebkalderon@gmail.com>
2025-08-25 19:54:11 -0400

    Fix broken yaml-language-server settings in Neovim

--- neovim/config/.config/nvim/lsp/yamlls.lua
+++ neovim/config/.config/nvim/lsp/yamlls.lua
@@ -3,14 +3,22 @@
 ---@type vim.lsp.Config
 return {
   cmd = { "yaml-language-server", "--stdio" },
-  filetypes = { "yaml", "yaml.docker-compose", "yaml.gitlab" },
+  filetypes = { "yaml", "yaml.docker-compose", "yaml.gitlab", "yaml.helm-values" },
   root_markers = { ".git" },
+  on_init = function(client)
+    --- https://github.com/neovim/nvim-lspconfig/pull/4016
+    ---
+    --- Since formatting is disabled by default if you check
+    --- `client:supports_method('textDocument/formatting')` during `LspAttach`
+    --- it will return `false`. This hack sets the capability to `true` to
+    --- facilitate autocmd's which check this capability
+    client.server_capabilities.documentFormattingProvider = true
+  end,
   settings = {
-    -- https://github.com/redhat-developer/vscode-redhat-telemetry#how-to-disable-telemetry-reporting
     redhat = { telemetry = { enabled = false } },
+    yaml = { format = { enable = true } },
     schemas = {
       ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-      ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.yml",
     },
   },
 }
