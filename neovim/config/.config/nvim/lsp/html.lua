c8e8e6bf4ccafbed1f486d5b12065d00660cb406
Eyal Kalderon <ebkalderon@gmail.com>
2025-08-25 20:07:41 -0400

    Fix broken html-language-server settings in Neovim

--- neovim/config/.config/nvim/lsp/html.lua
+++ neovim/config/.config/nvim/lsp/html.lua
@@ -5,12 +5,18 @@ return {
   cmd = { "vscode-html-language-server", "--stdio" },
   filetypes = { "html", "htmldjango", "htmlangular" },
   root_markers = { ".git" },
-  settings = {
-    configurationSection = { "html", "css", "javascript" },
-    embeddedLanguages = {
-      css = true,
-      javascript = true,
+  capabilities = {
+    textDocument = {
+      completion = {
+        completionItem = {
+          snippetSupport = true,
+        },
+      },
     },
+  },
+  init_options = {
+    configurationSection = { "html", "css", "javascript" },
+    embeddedLanguages = { css = true, javascript = true },
     provideFormatter = true,
   },
 }
