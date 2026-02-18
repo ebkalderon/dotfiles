136fc6dfb84336766f60dfafae9259c80c4b46f3
Eyal Kalderon <ebkalderon@gmail.com>
2026-02-18 12:50:26 -0500

    Update nvim-treesitter config to enable TS features
    It turns out that the rewritten version of the `nvim-treesitter` plugin
    doesn't enable any TS-powered highlighting/folding automatically, so
    this commit adds an autocmd which starts `vim.treesitter` whenever a
    supported filetype is opened.

--- neovim/config/.config/nvim/lua/ebk/plugins/treesitter.lua
+++ neovim/config/.config/nvim/lua/ebk/plugins/treesitter.lua
@@ -6,9 +6,55 @@ return {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
     build = ":TSUpdate",
-    config = function()
+    opts = {
+      "bash",
+      "c",
+      "cpp",
+      "diff",
+      "dockerfile",
+      "gitignore",
+      "groovy",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "lua",
+      "luadoc",
+      "markdown",
+      "markdown_inline",
+      "rust",
+      "scss",
+      "sql",
+      "toml",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
+    },
+    config = function(_, opts)
       require("nvim-treesitter").setup()
-      require("nvim-treesitter").install({ "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" })
+      require("nvim-treesitter").install(opts):wait(300000)
+
+      local patterns = {}
+      for _, parser in ipairs(opts) do
+        local parser_patterns = vim.treesitter.language.get_filetypes(parser)
+        for _, pp in pairs(parser_patterns) do
+          table.insert(patterns, pp)
+        end
+      end
+
+      vim.api.nvim_create_autocmd("FileType", {
+        desc = "Enable Treesitter features for all supported languages",
+        pattern = patterns,
+        callback = function()
+          -- Enable syntax highlighting
+          vim.treesitter.start()
+
+          -- Enable augmented code folding
+          vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+          vim.wo[0][0].foldmethod = "expr"
+        end,
+      })
     end,
   },
 }
