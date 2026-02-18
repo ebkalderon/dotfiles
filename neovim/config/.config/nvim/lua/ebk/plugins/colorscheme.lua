080aab7b16947a8a3f5692fc4efe1bfe49cbcdde
Eyal Kalderon <ebkalderon@gmail.com>
2026-02-18 17:28:38 -0500

    Update monokai-pro.nvim config to match upstream README

--- neovim/config/.config/nvim/lua/ebk/plugins/colorscheme.lua
+++ neovim/config/.config/nvim/lua/ebk/plugins/colorscheme.lua
@@ -10,12 +10,10 @@ return {
   {
     "loctvl842/monokai-pro.nvim",
     lazy = false,
+    priority = 1000,
     config = function()
-      require("monokai-pro").setup({
-        filter = "classic",
-      })
-
-      vim.cmd([[colorscheme monokai-pro-classic]])
+      require("monokai-pro").setup({ filter = "classic" })
+      vim.cmd.colorscheme("monokai-pro")
     end,
   },
 }
