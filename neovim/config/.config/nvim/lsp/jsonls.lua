b989833a2a2eb959d30b47a674c77bfb144b48e7
Eyal Kalderon <ebkalderon@gmail.com>
2026-08-19 15:46:47 -0400

    Validate more JSON schemas in Neovim

--- neovim/config/.config/nvim/lsp/jsonls.lua
+++ neovim/config/.config/nvim/lsp/jsonls.lua
@@ -8,10 +8,31 @@ return {
   settings = {
     json = {
       format = { enable = true },
+      validate = { enable = true },
       schemas = {
         {
+          fileMatch = { "compile_commands.json" },
+          url = "https://json.schemastore.org/compile-commands.json",
+        },
+        {
+          fileMatch = { "dockerd.json" },
+          url = "https://json.schemastore.org/dockerd.json",
+        },
+        {
+          fileMatch = { "feed.json" },
+          url = "https://json.schemastore.org/feed.json",
+        },
+        {
+          fileMatch = { "jsconfig.json" },
+          url = "https://json.schemastore.org/jsconfig.json",
+        },
+        {
           fileMatch = { "package.json" },
-          url = "https://json.schemastore.org/package.json"
+          url = "https://json.schemastore.org/package.json",
+        },
+        {
+          fileMatch = { "tsconfig.json" },
+          url = "https://json.schemastore.org/tsconfig.json",
         },
       },
     },
